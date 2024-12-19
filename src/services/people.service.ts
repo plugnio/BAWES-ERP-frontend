0c232367dd2e1f141c286d73e4b31367d83ad58a
Khalid Al-Mutawa <khalid@bawes.net>
2024-12-20 02:54:00 +0300

    Fix errors

--- src/services/people.service.ts
+++ src/services/people.service.ts
@@ -1,5 +1,6 @@
 import { BaseService } from './base.service';
 import type { CreatePersonDto, UpdatePersonDto } from '@bawes/erp-api-sdk';
+import type { AxiosPromise } from 'axios';
 
 interface Person {
   id: string;
@@ -13,31 +14,31 @@ interface Person {
 export class PeopleService extends BaseService {
   async create(createPersonDto: CreatePersonDto): Promise<Person> {
     return this.handleRequest<Person>(
-      this.client.people.personControllerCreate(createPersonDto)
+      this.client.people.personControllerCreate(createPersonDto) as unknown as AxiosPromise<Person>
     );
   }
 
   async findAll(): Promise<Person[]> {
     return this.handleRequest<Person[]>(
-      this.client.people.personControllerFindAll()
+      this.client.people.personControllerFindAll() as unknown as AxiosPromise<Person[]>
     );
   }
 
   async findOne(id: string): Promise<Person> {
     return this.handleRequest<Person>(
-      this.client.people.personControllerFindOne(id)
+      this.client.people.personControllerFindOne(id) as unknown as AxiosPromise<Person>
     );
   }
 
   async update(id: string, updatePersonDto: UpdatePersonDto): Promise<Person> {
     return this.handleRequest<Person>(
-      this.client.people.personControllerUpdate(id, updatePersonDto)
+      this.client.people.personControllerUpdate(id, updatePersonDto) as unknown as AxiosPromise<Person>
     );
   }
 
   async remove(id: string): Promise<void> {
     await this.handleRequest<void>(
-      this.client.people.personControllerRemove(id)
+      this.client.people.personControllerRemove(id) as unknown as AxiosPromise<void>
     );
   }
 } 
