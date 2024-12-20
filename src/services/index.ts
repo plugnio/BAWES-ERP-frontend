831e9675d3319c54528211007f6b2cb9b549d1f9
Khalid Al-Mutawa <khalid@bawes.net>
2024-12-20 04:53:51 +0300

    Implement service discovery

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,33 @@
-export { BaseService } from './base.service';
-export { AuthService } from './auth.service';
-export { PeopleService } from './people.service';
-export { PermissionsService } from './permissions.service'; 
+import { BaseService } from './base.service';
+import { AuthService } from './auth.service';
+import { PeopleService } from './people.service';
+import { PermissionsService } from './permissions.service';
+
+export interface Services {
+  auth: AuthService;
+  people: PeopleService;
+  permissions: PermissionsService;
+}
+
+class ServiceRegistry {
+  private static instance: ServiceRegistry;
+  
+  readonly auth: AuthService;
+  readonly people: PeopleService;
+  readonly permissions: PermissionsService;
+
+  private constructor() {
+    this.auth = new AuthService();
+    this.people = new PeopleService();
+    this.permissions = new PermissionsService();
+  }
+
+  static getInstance(): ServiceRegistry {
+    if (!ServiceRegistry.instance) {
+      ServiceRegistry.instance = new ServiceRegistry();
+    }
+    return ServiceRegistry.instance;
+  }
+}
+
+export const getServices = (): Services => ServiceRegistry.getInstance(); 
